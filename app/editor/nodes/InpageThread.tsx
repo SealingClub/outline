d1c42e5a4256720ae3421e0d437f5ebcc781f072
vcup <miryo@vcup.moe>
2024-02-10 19:03:06 +0800

    chore: format codes

--- app/editor/nodes/InpageThread.tsx
+++ app/editor/nodes/InpageThread.tsx
@@ -1,33 +1,35 @@
-import InpageThread from "@shared/editor/nodes/InpageThread"
+import throttle from "lodash/throttle";
 import React, { useEffect, useState, useContext, useMemo } from "react";
-import { ComponentProps } from "@shared/editor/types";
-import useStores from "~/hooks/useStores";
-
+import { useTranslation } from "react-i18next";
 import { useRouteMatch } from "react-router-dom";
-import { Reply, Thread } from "~/scenes/Document/components/CommentThread";
-import { ResizingHeightContainer } from "~/components/ResizingHeightContainer";
+import styled from "styled-components";
+import InpageThread from "@shared/editor/nodes/InpageThread";
+import { ComponentProps } from "@shared/editor/types";
+import { ProsemirrorData } from "@shared/types";
 import CommentForm from "~/scenes/Document/components/CommentForm";
+import { Reply, Thread } from "~/scenes/Document/components/CommentThread";
+
+import CommentThreadItem from "~/scenes/Document/components/CommentThreadItem";
+import Avatar from "~/components/Avatar";
+import { useDocumentContext } from "~/components/DocumentContext";
 import Fade from "~/components/Fade";
 import Flex from "~/components/Flex";
-import usePolicy from "~/hooks/usePolicy";
-import CommentThreadItem from "~/scenes/Document/components/CommentThreadItem";
-import { useTranslation } from "react-i18next";
-import Avatar from "~/components/Avatar";
+import { ResizingHeightContainer } from "~/components/ResizingHeightContainer";
 import Typing from "~/components/Typing";
-import { throttle } from "lodash";
 import { WebsocketContext } from "~/components/WebsocketProvider";
 import useCurrentUser from "~/hooks/useCurrentUser";
 import useOnClickOutside from "~/hooks/useOnClickOutside";
-import styled from "styled-components";
-import { useDocumentContext } from "~/components/DocumentContext";
 import usePersistedState from "~/hooks/usePersistedState";
-import { ProsemirrorData } from "@shared/types";
+import usePolicy from "~/hooks/usePolicy";
+import useStores from "~/hooks/useStores";
 
 // eslint-disable-next-line react/display-name
-InpageThread.prototype.component = function InpageThreadComponent(props: ComponentProps) {
+InpageThread.prototype.component = function InpageThreadComponent(
+  props: ComponentProps
+) {
   const { node } = props;
   const { editor } = useDocumentContext();
-  const { comments, documents } = useStores()
+  const { comments, documents } = useStores();
   const user = useCurrentUser();
   const { t } = useTranslation();
   const match = useRouteMatch<{ documentSlug: string }>();
@@ -36,27 +38,29 @@ InpageThread.prototype.component = function InpageThreadComponent(props: Compone
   const can = usePolicy(document.id);
   const [thread, setThread] = useState(comments.get(node.attrs.id));
   useEffect(() => {
-    const threadLocal = comments.get(node.attrs.id)
+    const threadLocal = comments.get(node.attrs.id);
     if (threadLocal) {
-      setThread(threadLocal)
-      return
+      setThread(threadLocal);
+      return;
     }
-    void (async () => setThread(
-      await comments.create({
-        id: node.attrs.id,
-        documentId: document.id,
-        data: node,
-        isInpage: true,
-      })
-    ))()
-  }, [setThread])
-
+    void (async () =>
+      setThread(
+        await comments.create({
+          id: node.attrs.id,
+          documentId: document.id,
+          data: node,
+          isInpage: true,
+        })
+      ))();
+  }, [comments, setThread, document, node]);
 
   const socket = useContext(WebsocketContext);
   const setIsTyping = useMemo(
     () =>
       throttle(() => {
-        if (!thread) return
+        if (!thread) {
+          return;
+        }
         socket?.emit("typing", {
           documentId: document.id,
           commentId: thread.id,
@@ -65,12 +69,13 @@ InpageThread.prototype.component = function InpageThreadComponent(props: Compone
     [socket, document.id, thread]
   );
 
-
   const [focused, setFocused] = useState(thread?.isNew ?? false);
 
   const topRef = React.useRef<HTMLDivElement>(node.attrs.id);
-  useOnClickOutside(topRef, (event) => {
-    if (focused) setFocused(false)
+  useOnClickOutside(topRef, (_) => {
+    if (focused) {
+      setFocused(false);
+    }
   });
 
   const [draft, onSaveDraft] = usePersistedState<ProsemirrorData | undefined>(
@@ -78,7 +83,9 @@ InpageThread.prototype.component = function InpageThreadComponent(props: Compone
     undefined
   );
 
-  if (!thread) return <div>no comments available</div>
+  if (!thread) {
+    return <div>no comments available</div>;
+  }
 
   const highlightedCommentMarks = editor
     ?.getComments()
@@ -86,69 +93,70 @@ InpageThread.prototype.component = function InpageThreadComponent(props: Compone
   const highlightedText = highlightedCommentMarks?.map((c) => c.text).join("");
 
   const commentsInThread = comments
-    .inThreadInpageOnly(thread?.id ?? '')
+    .inThreadInpageOnly(thread?.id ?? "")
     .filter((comment) => !comment.isNew && comment.parentCommentId);
 
-  return (<Thread
-    ref={topRef}
-    $focused={focused}
-    $recessed={false}
-    $dir={document.dir}
-  >
-    {commentsInThread.map((comment, index) => {
-      const firstOfAuthor =
-        index === 0 ||
-        comment.createdById !== commentsInThread[index - 1].createdById;
-      const lastOfAuthor =
-        index === commentsInThread.length - 1 ||
-        comment.createdById !== commentsInThread[index + 1].createdById;
+  return (
+    <Thread
+      ref={topRef}
+      $focused={focused}
+      $recessed={false}
+      $dir={document.dir}
+    >
+      {commentsInThread.map((comment, index) => {
+        const firstOfAuthor =
+          index === 0 ||
+          comment.createdById !== commentsInThread[index - 1].createdById;
+        const lastOfAuthor =
+          index === commentsInThread.length - 1 ||
+          comment.createdById !== commentsInThread[index + 1].createdById;
 
-      return (
-        <CommentThreadItem
-          highlightedText={index === 0 ? highlightedText : undefined}
-          comment={comment}
-          onDelete={() => editor?.removeComment(comment.id)}
-          key={comment.id}
-          firstOfThread={index === 0}
-          lastOfThread={index === commentsInThread.length - 1}
-          canReply={focused && can.comment}
-          firstOfAuthor={firstOfAuthor}
-          lastOfAuthor={lastOfAuthor}
-          previousCommentCreatedAt={commentsInThread[index - 1]?.createdAt}
-          dir={document.dir}
-        />
-      );
-    })}
-
-    {thread.currentlyTypingUsers
-      .filter((typing) => typing.id !== user.id)
-      .map((typing) => (
-        <Flex gap={8} key={typing.id}>
-          <Avatar model={typing} size={24} />
-          <Typing />
-        </Flex>
-      ))}
-    <ResizingHeightContainer hideOverflow={false}>
-      {(focused || commentsInThread.length === 0) && can.comment && (
-        <Fade timing={100}>
-          <StyledCommentForm
-            onSaveDraft={onSaveDraft}
-            draft={draft}
-            documentId={document.id}
-            thread={thread}
-            onTyping={setIsTyping}
-            standalone={commentsInThread.length === 0}
+        return (
+          <CommentThreadItem
+            highlightedText={index === 0 ? highlightedText : undefined}
+            comment={comment}
+            onDelete={() => editor?.removeComment(comment.id)}
+            key={comment.id}
+            firstOfThread={index === 0}
+            lastOfThread={index === commentsInThread.length - 1}
+            canReply={focused && can.comment}
+            firstOfAuthor={firstOfAuthor}
+            lastOfAuthor={lastOfAuthor}
+            previousCommentCreatedAt={commentsInThread[index - 1]?.createdAt}
             dir={document.dir}
-            autoFocus={focused}
           />
-        </Fade>
+        );
+      })}
+
+      {thread.currentlyTypingUsers
+        .filter((typing) => typing.id !== user.id)
+        .map((typing) => (
+          <Flex gap={8} key={typing.id}>
+            <Avatar model={typing} size={24} />
+            <Typing />
+          </Flex>
+        ))}
+      <ResizingHeightContainer hideOverflow={false}>
+        {(focused || commentsInThread.length === 0) && can.comment && (
+          <Fade timing={100}>
+            <StyledCommentForm
+              onSaveDraft={onSaveDraft}
+              draft={draft}
+              documentId={document.id}
+              thread={thread}
+              onTyping={setIsTyping}
+              standalone={commentsInThread.length === 0}
+              dir={document.dir}
+              autoFocus={focused}
+            />
+          </Fade>
+        )}
+      </ResizingHeightContainer>
+      {!focused && can.comment && (
+        <Reply onClick={() => setFocused(true)}>{t("Reply")}…</Reply>
       )}
-    </ResizingHeightContainer>
-    {!focused && can.comment && (
-      <Reply onClick={() => setFocused(true)}>{t("Reply")}…</Reply>
-    )}
-  </Thread>
-  )
+    </Thread>
+  );
 };
 
 const StyledCommentForm = styled(CommentForm)`
@@ -156,4 +164,4 @@ const StyledCommentForm = styled(CommentForm)`
   left: -4px;
 `;
 
-export default InpageThread
+export default InpageThread;
